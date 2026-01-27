df5d28daf8eb5a4d1967b867a5e6fadbcb05dfec
Reinhard Jung <github@reinhard.jung.name>
2026-01-27 15:15:33 +0100

    Mwsst Zeiel

--- cmx-misbuero.php
+++ cmx-misbuero.php
@@ -4,7 +4,7 @@
  * Plugin Name: CLOUD Meister - Mis Büro
  * Plugin URI: https://cloudmeister.ch/cmx-misbuero/
  * Description: Mis Büro by Reiny und Demamels.
- * Version: 25.0123.6511-5
+ * Version: 25.0123.6511-666
  * Text Domain: cmx-misbuero
  * Domain Path: /languages
  * Author: CLOUD Meister
