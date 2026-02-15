350f216de395e8a85267178b66d73d1bf922f616
Reinhard Jung <github@reinhard.jung.name>
2026-02-15 09:38:45 +0100

    Vor Artkel Spaltenbreite

--- cmx-misbuero.php
+++ cmx-misbuero.php
@@ -4,7 +4,7 @@
  * Plugin Name: CLOUD Meister - Mis Büro
  * Plugin URI: https://misbuero.ch/wp-content/uploads/cmx-misbuero.zip
  * Description: Mis Büro by CLOUD Meister.
- * Version: 2.15.927
+ * Version: 2.15.938
  * Text Domain: cmx-misbuero
  * Domain Path: /languages
  * Author: CLOUD Meister
