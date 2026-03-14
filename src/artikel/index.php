132c87a095cfc3546fbd27020abe8f636b4a9ce0
Reinhard Jung <github@reinhard.jung.name>
2026-03-14 23:36:24 +0100

    ALLES Andere NCIHT anfassen nur das folgende machen
    A: Bitte alles was diese metabxox betrifft in folgende datei auslagern: src/artikel/bilder.php
    B: Wenn eine Datei zu begin in der Liste steht kann der Pfeil für Hoch ausgeblendet werden, weil es ja nciht weiter höher in der liste geht
    C: Das gleiche bvetrifft die letzte Datei. Dort auch keinen runter Pfeil machen
    D: Sonst naatürlich immer beide Pfeile anzeigen
    E: NUR wenn etwas and en Bilder gewändert wurde: Als ein neues hinzugefügt wurde oder eines zum löschen markert wurde etc. Dann uner die Liste einen Button einblenden "Artikel speichern", damit mah noicht hoch scrollen muss um den Artikel spoecihern zu können

--- src/artikel/index.php
+++ src/artikel/index.php
@@ -37,7 +37,7 @@ cmx_define_meta_constants(basename(__DIR__), 'sku,ek,vk,marge,waehrungen,verkauf
 
 
 // Include: @ll metaboxes
-cmx_require_files(__DIR__,'stammdaten,lieferanten,belegtext,konditionen,admincolumns,doppelte,liste_artikel,liste_artikel detail,qr-code,exports,imports,infos,preview,status');
+cmx_require_files(__DIR__,'stammdaten,lieferanten,belegtext,konditionen,admincolumns,doppelte,liste_artikel,liste_artikel detail,qr-code,exports,imports,bilder,preview,status');
 
 
 // Halte den Slug immer synchron mit dem Titel beim Speichern.
