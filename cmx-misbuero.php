cae8280a0f9fab399d9875d4d337c551c77be6ae
Reinhard Jung <github@reinhard.jung.name>
2026-03-18 04:56:17 +0100

    In allen CPT sooll die TextArea be "Internn Notizen" um 3 Zeile weniger hoch sein

--- cmx-misbuero.php
+++ cmx-misbuero.php
@@ -4,7 +4,7 @@
  * Plugin Name: CLOUD Meister - Mis Büro
  * Plugin URI: https://misbuero.ch/wp-content/uploads/cmx-misbuero.zip
  * Description: Mis Büro by CLOUD Meister.
- * Version: 3.18.450
+ * Version: 3.18.454
  * Text Domain: cmx-misbuero
  * Domain Path: /languages
  * Author: CLOUD Meister
