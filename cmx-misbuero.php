d320015837bd39215fd6d8730c3c4ea436063887
Reinhard Jung <github@reinhard.jung.name>
2026-03-21 11:02:39 +0100

    Beiträge suchen

--- cmx-misbuero.php
+++ cmx-misbuero.php
@@ -4,7 +4,7 @@
  * Plugin Name: CLOUD Meister - Mis Büro
  * Plugin URI: https://misbuero.ch/wp-content/uploads/cmx-misbuero.zip
  * Description: Mis Büro by CLOUD Meister.
- * Version: 3.21.958
+ * Version: 3.21.1101
  * Text Domain: cmx-misbuero
  * Domain Path: /languages
  * Author: CLOUD Meister
