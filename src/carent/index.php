f1b465f1adf6a1ea98df8feeca8e4e2c50f25a17
Reinhard Jung <github@reinhard.jung.name>
2026-04-13 11:56:42 +0200

    Fotos=Vorne Links, Vorrne Rechts, Hinten links, Hinten Rechts, Dach, Kotlügel, Kofferaum

--- src/carent/index.php
+++ src/carent/index.php
@@ -272,7 +272,7 @@ register_post_type(basename(__DIR__), ['labels' => ['name' => 'CaRent', 'singula
 
 
 // Define: CONST 4 @ll Taxos
-define(__NAMESPACE__ . '\\CMX_TAX_'.strtoupper(basename(__DIR__)),'Kategorien');
+define(__NAMESPACE__ . '\\CMX_TAX_'.strtoupper(basename(__DIR__)),'Fotos'); // 'Kategorien,Fotos'
 
 
 // Define: CONST 4 each Taxo
@@ -282,7 +282,7 @@ cmx_const_taxos(strtoupper(basename(__DIR__)),basename(__DIR__), CMX_TAX_CARENT)
 
 // Create: @ll Taxos
 \add_action('init', function () {
-	// cmx_create_taxo(basename(__DIR__), 'Kategorie', 'Kategorien');
+	cmx_create_taxo(basename(__DIR__), 'Kategorie', 'Fotos');
 }, 15);
 
 
