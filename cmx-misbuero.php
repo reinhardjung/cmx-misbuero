0af911709a5c041af5f4d8db5ae9100adfe6745d
Reinhard Jung <github@reinhard.jung.name>
2026-05-10 06:06:30 +0200

    [versionen] standard=invoices,customers,qr_invoice,pdf,woocommerce_basic,settings,documents,projects,suppliers business=camt,automation,advanced_reports,emails modules=payrexx,facturx,treuhand,openai

--- cmx-misbuero.php
+++ cmx-misbuero.php
@@ -4,7 +4,7 @@
  * Plugin Name: CLOUD Meister - Mis Büro
  * Plugin URI: https://misbuero.ch/wp-content/uploads/cmx-misbuero.zip
  * Description: Mis Büro by CLOUD Meister.
- * Version: 5.10.554
+ * Version: 5.10.604
  * Text Domain: cmx-misbuero
  * Domain Path: /languages
  * Author: CLOUD Meister
